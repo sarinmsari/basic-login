65d8088f17891ce8b02cf702abe08ab2b151ded1
Sarin <sarinmsari@gmail.com>
2020-07-27 14:01:29 +0530

    dashboard added

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import './style.css'
+import "./style.css";
 
 function dashboard() {
-    return(
-        <div>
-            dashboard
-        </div>
-    );
+  return (
+    <div className="container">
+      <Link to="/login" className="logout">Logout</Link>
+      <div className="header">Dashboard</div>
+      <div className="content">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam odit
+        tempora quam quidem quasi possimus dicta! Laboriosam quas tempora a
+        tempore, quidem harum consequuntur asperiores dolor non, maxime
+        provident dignissimos.
+      </div>
+      <div className="content">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam odit
+        tempora quam quidem quasi possimus dicta! Laboriosam quas tempora a
+        tempore, quidem harum consequuntur asperiores dolor non, maxime
+        provident dignissimos.
+      </div>
+      <div className="content">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam odit
+        tempora quam quidem quasi possimus dicta! Laboriosam quas tempora a
+        tempore, quidem harum consequuntur asperiores dolor non, maxime
+        provident dignissimos.
+      </div>
+    </div>
+  );
 }
 
 export default dashboard;
